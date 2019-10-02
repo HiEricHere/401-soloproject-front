bc279d7d13aa62699e654095b8c4c37cbba8e65c
Eric Huang <ejhuang84@gmail.com>
2019-10-01 23:27:41 -0700

    added dependencies, scaffolding

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => {
-  return <h1>My React App!!</h1>;
-};
+import App from './components/app';
 
 const root = document.getElementById('root');
 ReactDOM.render(<App />, root);
